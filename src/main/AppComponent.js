55a777c08cdf5e3020e708d714e2c10510aa3ed1
Alejandro Quiroz Serna <jakop.box@gmail.com>
2018-10-27 22:05:53 -0500

    Added tail wind and fixed structure

--- src/main/AppComponent.js
+++ src/main/AppComponent.js
@@ -2,13 +2,37 @@ import React, { Component } from 'react';
 import Content from "./content";
 
 class AppComponent extends Component {
-  render() {
-    return (
-      <div className="App w-full h-full">
-        <Content/>
-      </div>
-    );
-  }
+    state = {
+        currentStep : 0,
+        userData : {
+            email: null,
+            cellphone: null,
+            fullName: null,
+        }
+    };
+
+    setUserData({fullName=null, email=null, cellphone=null}) {
+        this.setState({
+            email,
+            cellphone,
+            fullName,
+        });
+    }
+
+    changeStep(step) {
+
+    }
+
+    render() {
+        return (
+            <div className="App w-full h-full">
+                <Content
+                    setUserData={this.setUserData.bind(this)}
+                    changeStep={this.changeStep.bind(this)}
+                />
+            </div>
+        );
+    }
 }
 
 export default AppComponent;
